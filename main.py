eef493f8485b456e9c707a4ac3b4821fde99e4ce
ProvenceStar <ProvenceStar>
2024-11-03 13:41:15 +0800

    base code

--- main.py
+++ main.py
@@ -107,9 +107,9 @@ test_data = replace_mult_event(test_data,reverse_event_dict)
 
 
 # ---------- network ----------
-# net = MLP(args).to(device)
+net = MLP(args).to(device)
 # net = SeDGPL(args).to(device)
-net = SeDGPL1(args).to(device)
+# net = SeDGPL1(args).to(device)
 net.handler(to_add, tokenizer)
 
 if args.ckpt.strip() != '':
